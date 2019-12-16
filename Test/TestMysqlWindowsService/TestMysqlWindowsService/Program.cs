1013a5d204a9a4ed28541f1227f20be0ef2be336
zdyu <1877682825@qq.com>
2019-12-16 10:48:17 +0800

    lito

---
+++ Test/TestMysqlWindowsService/TestMysqlWindowsService/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+using Microsoft.Extensions.Logging;
+
+namespace TestMysqlWindowsService
+{
+    class Program
+    {
+        // P/Invoke declarations for Windows.
+        [DllImport("kernel32.dll")] static extern IntPtr GetConsoleWindow();
+        [DllImport("user32.dll")] static extern bool IsWindowVisible(IntPtr hWnd);
+
+        public static bool HaveVisibleConsole()
+        {
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ?
+                IsWindowVisible(GetConsoleWindow())
+                :
+                Console.WindowHeight > 0;
+        }
+
+
+        static async Task Main(string[] args)
+        {
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+            if (HaveVisibleConsole()) isService = false;
+
+
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+                Directory.SetCurrentDirectory(pathToContentRoot);
+                CreateWebHostBuilder(args).Build().RunAsService();
+            }
+            else
+            {
+                await CreateWebHostBuilder(args).Build().RunAsync();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging => { logging.ClearProviders(); })
+                .UseUrls("http://*:5000");
+        }
+    }
+}
